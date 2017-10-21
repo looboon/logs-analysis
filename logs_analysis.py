327bb0e4eae1cf407c9ffbb25542b32d8943a752
Loo Choon Boon <loochoonboon@live.com>
2017-10-22 02:46:56 +0800

    Resubmit logs_analysis.py and sample-output

--- logs_analysis.py
+++ logs_analysis.py
@@ -2,19 +2,26 @@
 import psycopg2
 
 
+def get_query_results(query):
+    db = psycopg2.connect(dbname="news")
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    c.close()
+    db.close()
+    return results
+
+
 def top_three():
-    conn = psycopg2.connect(dbname="news")
-    query = """SELECT SUBSTR(path, 10) AS article, COUNT(id) AS views
-               FROM log
-               GROUP BY 1
-               HAVING SUBSTR(path, 10) <> ''
-               ORDER BY 2 DESC
+    query = """SELECT b.title, COUNT(a.id) AS views
+               FROM log AS a
+               INNER JOIN articles AS b
+               ON SUBSTR(a.path, 10) = b.slug
+               WHERE SUBSTR(path, 10) <> ''
+               GROUP BY title
+               ORDER BY views DESC
                LIMIT 3"""
-    cursor = conn.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    results = get_query_results(query)
 
     # return results
     final_output = "----- Most popular three articles of all time --------\n"
@@ -27,18 +34,13 @@ def top_three():
 
 
 def most_popular_authors():
-    conn = psycopg2.connect(dbname="news")
     query = """SELECT name, COUNT(c.id) AS views
                FROM authors AS a
                INNER JOIN articles AS b ON a.id = b.author
                LEFT JOIN log AS c ON SUBSTR(c.path, 10) = b.slug
                GROUP BY 1
                ORDER BY 2 DESC"""
-    cursor = conn.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    results = get_query_results(query)
 
     # return results
     final_output = "----- Most popular article authors of all time --------\n"
@@ -51,7 +53,6 @@ def most_popular_authors():
 
 
 def error_list():
-    conn = psycopg2.connect(dbname="news")
     query = """SELECT DATE(a.time) AS log_date,
                (COUNT(a.status)/AVG(all_count) * 100) AS percent_errors
                FROM log AS a
@@ -62,11 +63,7 @@ def error_list():
                WHERE status = '404 NOT FOUND'
                GROUP BY 1
                HAVING (COUNT(a.status)/AVG(all_count) * 100) > 1"""
-    cursor = conn.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    results = get_query_results(query)
 
     # return results
     final_output = "----- Days where more than 1% of " \
